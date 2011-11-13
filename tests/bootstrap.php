1992aaf62b1736b562d97fcf7f0c28644c9a19a4
Daniele Alessandri <suppakilla@gmail.com>
2011-11-13 19:28:37 +0100

    Fix Pimple not loading when running the test suite.
    This is a temporary fix since Pimple does not use Composer autoloading yet.
    
    We are not going to push a new patch release since it does not affect the
    actual service provider.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -10,4 +10,5 @@
  */
 
 require_once __DIR__.'/../vendor/.composer/autoload.php';
+require_once __DIR__.'/../vendor/pimple/pimple/lib/Pimple.php';
 require_once __DIR__.'/../lib/Predis/Silex/PredisServiceProvider.php';
